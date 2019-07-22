105703aa269dfd097ccafb01e35f647f8c6ef015
Дорджиев Мингиян Вячеславович <Dordzhiev.M.V@sberbank.ru>
2019-07-22 15:09:09 +0300

    add init_ddl.sql

---
+++ resources/init_ddl.sql
@@ -0,0 +1,21 @@
+create table currency_tbl (
+  id serial primary key not null,
+  name varchar(20) not null
+);
+
+create table account_tbl (
+  id serial primary key not null,
+  name varchar(50) not null,
+  amount decimal(15, 2) not null,
+  currency_id int not null
+    
+);
+
+ALTER TABLE account_tbl
+ADD CONSTRAINT currency_fk
+FOREIGN KEY (currency_id) REFERENCES public.currency_tbl (id);
+
+create table category_tbl (
+  id serial primary key not null,
+  name varchar(30) not null
+);
