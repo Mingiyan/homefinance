2ce0968dd63b4384b20131e113e77e4c5e6571c1
mingiyandordzhiev <vvvdmingiyan@gmail.com>
2019-08-02 00:20:17 +0300

    edit class in test package

--- dao/src/test/java/ru/geekfactory/homefinance/dao/repository/AccountRepositoryTest.java
+++ dao/src/test/java/ru/geekfactory/homefinance/dao/repository/AccountRepositoryTest.java
@@ -1,4 +1,15 @@
 package ru.geekfactory.homefinance.dao.repository;
 
+import org.junit.jupiter.api.BeforeAll;
+import ru.geekfactory.homefinace.dao.model.AccountModel;
+import ru.geekfactory.homefinace.dao.repository.RepositoryCRUD;
+
 public class AccountRepositoryTest {
+
+    private RepositoryCRUD<Long, AccountModel> accountModel;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("BeforeAll step.");
+    }
 }
