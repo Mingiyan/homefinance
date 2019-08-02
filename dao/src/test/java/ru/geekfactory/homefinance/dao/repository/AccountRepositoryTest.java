e0c43d2bc088acf6dc665d3d205e72baf5539769
Дорджиев Мингиян Вячеславович <Dordzhiev.M.V@sberbank.ru>
2019-08-02 14:27:19 +0300

    try create test

--- dao/src/test/java/ru/geekfactory/homefinance/dao/repository/AccountRepositoryTest.java
+++ dao/src/test/java/ru/geekfactory/homefinance/dao/repository/AccountRepositoryTest.java
@@ -3,20 +3,22 @@ package ru.geekfactory.homefinance.dao.repository;
 import org.junit.jupiter.api.*;
 import ru.geekfactory.homefinace.dao.repository.AccountRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AccountRepositoryTest {
 
-    private AccountRepository accountRepository;
+    private DatabaseConnectorTest databaseConnectorTest;
+    private AccountRepository accountRepository = new AccountRepository();
 
     @BeforeAll
-    static void beforeAll() {
-        System.out.println("BeforeAll step.");
+    static void beforeAll(DatabaseConnectorTest databaseConnectorTest) {
+        databaseConnectorTest.initDB();
     }
+
 
     @BeforeEach
     void beforeEach() {
-        accountRepository = new AccountRepository();
+        databaseConnectorTest = new DatabaseConnectorTest();
     }
 
     @Test
---
+++ dao/src/test/java/ru/geekfactory/homefinance/dao/repository/DatabaseConnectorTest.java
@@ -0,0 +1,38 @@
+package ru.geekfactory.homefinance.dao.repository;
+
+import org.h2.tools.RunScript;
+import ru.geekfactory.homefinace.dao.HomeFinanceDaoException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnectorTest {
+    private final static String DB_URL = "jdbc:h2:mem:geekfactoryTest";
+    private final static String DB_USER = "sa";
+    private final static String DB_PASSWORD = "";
+
+    public Connection getConnection() {
+        try {
+            Connection connectionTest = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            connectionTest.setAutoCommit(false);
+            return connectionTest;
+        } catch (SQLException e) {
+            throw new HomeFinanceDaoException("Connection failed!");
+        }
+    }
+
+    public void initDB() {
+        try (Connection connection = getConnection()){
+            File script = new File(getClass().getResource("/init_ddl.sql").getFile());
+            RunScript.execute(connection, new FileReader(script));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Could not initialize with script");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
