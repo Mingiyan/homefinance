69c3356e8a6d556fb618a0e4ac89186c37df39b4
Mingiyan Dordzhiev <vvvdmingiyan@gmail.ru>
2019-08-20 21:12:24 +0300

    edit SaxParser

--- dao/src/main/java/ru/geekfactory/homefinance/dao/parsers/xml/SaxParser.java
+++ dao/src/main/java/ru/geekfactory/homefinance/dao/parsers/xml/SaxParser.java
@@ -6,19 +6,23 @@ import ru.geekfactory.homefinance.dao.model.TransactionModel;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 import java.util.List;
 
 public class SaxParser implements Parser<TransactionModel> {
 
     @Override
-    public List<TransactionModel> parserEntity(String xmlFile) throws ParserConfigurationException, IOException, SAXException, XMLStreamException {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        SAXParser parser = factory.newSAXParser();
+    public List<TransactionModel> parserEntity(String xmlFile) {
+
         SaxHandler saxHandler = new SaxHandler();
 
-        parser.parse(ClassLoader.getSystemResourceAsStream(xmlFile), saxHandler);
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            parser.parse(ClassLoader.getSystemResourceAsStream(xmlFile), saxHandler);
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
 
         return saxHandler.transactionsList;
     }
