fce74f679fe2a3921d57e486c8125032074d3552
Дорджиев Мингиян Вячеславович <Dordzhiev.M.V@sberbank.ru>
2019-08-26 15:32:12 +0300

    edit config

--- web/src/main/java/ru/geekfactory/homefinance/web/servlet/TransactionServlet.java
+++ web/src/main/java/ru/geekfactory/homefinance/web/servlet/TransactionServlet.java
@@ -1,7 +1,16 @@
 package ru.geekfactory.homefinance.web.servlet;
 
 import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
 
+@WebServlet("/transaction")
 public class TransactionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 
 }
