2e2f3c51450e79aa17df276887a576bb00a81502
Дорджиев Мингиян Вячеславович <Dordzhiev.M.V@sberbank.ru>
2019-08-28 15:29:08 +0300

    edit encoding

--- web/src/main/java/ru/geekfactory/homefinance/web/servlet/TransactionServlet.java
+++ web/src/main/java/ru/geekfactory/homefinance/web/servlet/TransactionServlet.java
@@ -16,7 +16,7 @@ public class TransactionServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
         TransactionService transactionService = new TransactionService();
         List<TransactionModel> list = transactionService.findAll();
         for (TransactionModel transaction : list) {
