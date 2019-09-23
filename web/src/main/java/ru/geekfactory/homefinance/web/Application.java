17ba474cf73408cebc859892f98d2ad1e129d5f8
mingiyandordzhiev <vvvdmingiyan@gmail.com>
2019-09-23 23:30:58 +0300

    add annotation for test

--- web/src/main/java/ru/geekfactory/homefinance/web/Application.java
+++ web/src/main/java/ru/geekfactory/homefinance/web/Application.java
@@ -1,5 +1,8 @@
 package ru.geekfactory.homefinance.web;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
 
