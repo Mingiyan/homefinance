d7479d554b4561b70ddbaf517371d99e65a994ca
Mingiyan Dordzhiev <vvvdmingiyan@gmail.ru>
2019-09-25 23:40:19 +0300

    edit test

--- dao/src/main/java/ru/geekfactory/homefinance/dao/model/Transaction.java
+++ dao/src/main/java/ru/geekfactory/homefinance/dao/model/Transaction.java
@@ -3,7 +3,6 @@ package ru.geekfactory.homefinance.dao.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
--- dao/src/test/java/ru/geekfactory/homefinance/dao/repository/AccountRepositoryTest.java
+++ dao/src/test/java/ru/geekfactory/homefinance/dao/repository/AccountRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {DaoConfig.class})
-@Transactional
 class AccountRepositoryTest {
 
     @Autowired
