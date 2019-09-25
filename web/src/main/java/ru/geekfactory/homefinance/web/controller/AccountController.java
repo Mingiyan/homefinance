324c7535f3bd1a32f999335066034db0fc5153d9
Дорджиев Мингиян Вячеславович <Dordzhiev.M.V@sberbank.ru>
2019-09-25 15:48:59 +0300

    account mvc is ready

--- web/src/main/java/ru/geekfactory/homefinance/web/controller/AccountController.java
+++ web/src/main/java/ru/geekfactory/homefinance/web/controller/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ru.geekfactory.homefinance.dao.model.Account;
 import ru.geekfactory.homefinance.dao.model.AccountType;
+import ru.geekfactory.homefinance.dao.model.Currency;
 import ru.geekfactory.homefinance.service.AccountService;
 import ru.geekfactory.homefinance.service.CurrencyService;
 
@@ -28,7 +29,10 @@ public class AccountController {
     @GetMapping("/account")
     public String getAccounts(Model model) {
         List<Account> accounts = accountService.findAll();
+        List<Currency> currencies = currencyService.findAll();
+        model.addAttribute("currencies", currencies);
         model.addAttribute("accounts", accounts);
+        model.addAttribute("accountTypes", AccountType.values());
         return "account";
     }
 
