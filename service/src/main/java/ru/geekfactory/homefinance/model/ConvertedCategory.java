33ec29b65385b8549ec7a9122b7d9aec8dc81924
mingiyandordzhiev <vvvdmingiyan@gmail.com>
2019-10-08 21:51:18 +0300

    add converted classes

--- service/src/main/java/ru/geekfactory/homefinance/model/ConvertedCategory.java
+++ service/src/main/java/ru/geekfactory/homefinance/model/ConvertedCategory.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class ConvertedCategory {
 }
